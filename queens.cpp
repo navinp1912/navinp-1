0a63946fcdfc88822895a7fdd3554c3ddaca6ad2
Navin S Parakkal <navinp1281@gmail.com>
2013-12-21 15:34:34 +0530

    Fast queens

--- queens.cpp
+++ queens.cpp
@@ -7,73 +7,91 @@ using namespace std;
 const int MAX = 55;
 char arr[MAX][6];
 int lasthash[MAX][6];
-int dp[50][5][32][32][5][32];
 const int MOD = 1000000007;
+
+
+int dp[MAX][1<<5][1<<5][1<<5];
 
 int N, M;
 
-
-int go (int row, int col, int lmask, int rmask,int lastcol,int colmask)
+int
+isgood (int row, int curmask, int prevmask, int lmask, int rmask,
+	int curbadmask)
 {
-  int i, j;
+  if (curmask & curbadmask)
+    return 0;
+  if (curmask & prevmask)
+    return 0;
+  if (curmask & (lmask))
+    return 0;
+  if (curmask & (rmask))
+    return 0;
+  vector < int >v;
+  int i;
+  for (i = 0; i < M; i++)
+    if (curmask & (1 << i))
+      v.push_back (i);
 
-
-  if (col == M)
+  for (i = 0; i + 1 < v.size (); i++)
     {
-      row++;
-      lmask <<= 1;
-      rmask >>= 1;
-      col = 0;
-      lastcol = -1;
+      if ((v[i + 1] - v[i]) == 1)
+	return 0;
+      int x = v[i], y = v[i + 1];
+      int j;
+      for (j = x + 1; j < y; j++)
+	if (arr[row][j] == '#')
+	  break;
+      if (j == y)
+	return 0;
     }
-  //printf("DEBUG: row=%d col=%d lmask=%x rmask=%x lastcol=%d colmask=%x\n",row,col,lmask,rmask,lastcol,colmask);
+
+
+  return 1;
+}
+
+
+int
+go (int row, int prevmask, int lmask, int rmask)
+{
+  int i, sum = 0, curbadmask = 0;
+
+
   if (row == N)
     {
-//      if (colmask > 0)
-	{
-	  //if((colmask+1)==(1<<M)) printf ("@@@@ row=N %x %x\n", colmask);
-	  return 1;
-	}
-      return 0;
+      //for (i = 0; i < row; i++) printf ("row=%d , prevmask=%x\n", i, state[i]);
+      return 1;
     }
-  //printf ("row=%d,col=%d  \n", row, col);
-
-  lmask=lmask&((1<<M)-1);
-  int slastcol=lastcol;
-
-  if(slastcol == -1) slastcol=M-1;
-
-  if(dp[row][col][lmask][rmask][slastcol][colmask]!=-1) return dp[row][col][lmask][rmask][slastcol][colmask];
-
-  int sum = 0;
-  if (arr[row][col] == '#')
-    return dp[row][col][lmask][rmask][slastcol][colmask]= go (row, col + 1, lmask & ~(1 << col), rmask & ~(1 << col),col,colmask & ~(1 << col))%MOD;
+	lmask&=((1<<M)-1);
+	rmask&=((1<<M)-1);
+	prevmask&=((1<<M)-1);
 
 
-  // printf(" LMASK=%d RMASK=%d\n",(lmask<<1)&(1<<col) , (rmask>>1)&(1<<col));
 
-  if ((((lmask) & (1 << col)) == 0) and (((rmask) & (1 << col)) == 0))
+  for (i = 0; i < M; i++)
+    if (arr[row][i] == '#')
+      curbadmask |= (1 << i);
+
+  	prevmask &= ~curbadmask;
+	lmask &=~curbadmask;
+	rmask &=~curbadmask;
+
+   if(dp[row][prevmask][lmask][rmask]!=-1) return dp[row][prevmask][lmask][rmask];
+  for (i = 0; i < (1 << M); i++)
     {
-      bool bad = false;
 
-      //printf("row=%d col==%d lasthash[row][col]=%d\n",row,col,lasthash[row][col]);
-
-      if ( lasthash[row][col] < lastcol ) bad=true;
-
-      if (colmask & (1 << col)) bad = true;
-
-
-      if (!bad)
+      if (isgood (row, i, prevmask, lmask, rmask, curbadmask))
 	{
-//	  printf (" choosing %d %d\n", row, col);
-	  sum += go (row, col + 1, (lmask) | (1 << col), (rmask) | (1 << col),col,  colmask | (1 << col))%MOD;
+	  //      printf("choosing row=%d,mask=%x\n",row,i);
+	  sum +=
+	    go (row + 1, prevmask | (i & (~curbadmask) & ((1 << M) - 1)),
+		(lmask | i) << 1, (rmask | i) >> 1);
+	  if(sum>=MOD) sum-=MOD;
 	}
     }
-    sum += go (row, col + 1, lmask, rmask,lastcol, colmask)%MOD;
 
-
-  return dp[row][col][lmask][rmask][slastcol][colmask]=sum%MOD;
+  return dp[row][prevmask][lmask][rmask]=sum;
 }
+
 
 
 int
@@ -84,40 +102,32 @@ main ()
   while (T--)
     {
       scanf (" %d %d", &N, &M);
-      int i,j;
+      int i, j;
       for (i = 0; i < N; i++)
 	scanf (" %s", arr[i]);
 
-	for(i=0;i<N;i++){
-	 int firstcol=-1;
-	for(j=0;j<M;j++){
-		lasthash[i][j]=firstcol;
-		if(arr[i][j]=='#') {
-			firstcol=j;
-		}
-			/*
-			for(int lmask=0;lmask<(1<<M);lmask++)
-			for(int rmask=0;rmask<(1<<M);rmask++)
-		  	for(int k=0;k<M;k++)
-			for(int colmask=0;colmask<(1<<M);colmask++)
-				dp[i][j][lmask][rmask][k][colmask]=-1;
-			*/
-		
+      for (i = 0; i < N; i++)
+	{
+	  int firstcol = -1;
+	  for (j = 0; j < M; j++)
+	    {
+	      lasthash[i][j] = firstcol;
+	      if (arr[i][j] == '#')
+		{
+		  firstcol = j;
 		}
 
-	  
+	    }
 	}
 
-	memset(dp,-1,sizeof(dp));
 
       int ans = 0;
 
-
+	memset(dp,-1,sizeof(dp));
       i = j = 0;
-      ans = go (i, j, 0, 0,-1, 0)%MOD;
-      ans--;
-      if(ans<0) ans+=MOD;
-
+      ans = go (0, 0, 0, 0);
+	ans--;
+	if(ans<0) ans+=MOD;
       printf ("%d\n", ans);
     }
   return 0;
